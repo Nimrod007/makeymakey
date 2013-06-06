e84e705690f20ad4acdec1f589d63a83133876f5
Nimrod Lahav <nimrod.lahav@gmail.com>
2013-06-06 22:40:58 +0300

    updated readme

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ makeymakey
 ==========
 
 makeymakey web game
+
+This project will hold various web games (html,php) for makey makey usage
+
--- keyStroke.php
+++ keyStroke.php
@@ -1,4 +1,20 @@
 <?php
-echo "hi";
-
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+document.addEventListener('keydown', function(event) {
+
+        var key =  String.fromCharCode(event.keyCode);
+        var fieldNameElement = document.getElementById('key');
+        fieldNameElement.innerHTML = key;
+
+}, true);
+</script>
+</head>
+<body>
+<div id="key">press a key</div>
+</body>
+</html>
